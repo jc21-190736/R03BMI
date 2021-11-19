e4aecd80738c9e32af226be45b40410728198b39
龍成 小林 <190736@jc-21.jp>
2021-11-19 11:01:33 +0900

    BMI修正

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -20,25 +20,79 @@ namespace R03BMI
         {
             double d1 = double.Parse("5.12345");
 
-            string height = weighit.Text;
-            string weight = heighit.Text;
+            string weight = weighit.Text;
+            string height = heighit.Text;
 
                 double h = double.Parse(height);
                 double w = double.Parse(weight);
+
+
 
                 if (h > 10)
                 {
                     h = h / 100;
                     double b = w / h / h;
-                    result.Text = "身長" + h*100 + "cm、体重" + w + "kgの人のBMIは" + Math.Round(b * 10) / 10 + "です。";
+
+                double BMI = Math.Round(b * 10) / 10;
+           if (BMI >= 40.00)
+            {
+                b="肥満(4度)";
+            }
+            else if(BMI >= 35.00)
+            {
+                b="肥満(3度)";
+            }
+            else if(BMI >= 30.00)
+            {
+                b="肥満(2度)";
+            }
+            else if(BMI >= 25.00)
+            {
+                b="肥満(1度)";
+            }
+            else if(BMI >= 18.50)
+            {
+                b="普通体重";
+            }
+            else 
+            {
+                b="低体重";
+            }
+                    result.Text = "身長" + h*100 + "cm、体重" + w + "kgの人のBMIは" +BMI + "です。";
 
                 }
                 else
                 {
 
                     double b = w / h / h;
+                double BMI = Math.Round(b * 10) / 10;
 
-                    result.Text = "身長" + h + "m、体重" + w + "kgの人のBMIは" + Math.Round(b * 10) / 10 + "です。";
+            if (BMI >= 40.00)
+            {
+                b="肥満(4度)";
+            }
+            else if(BMI >= 35.00)
+            {
+                b="肥満(3度)";
+            }
+            else if(BMI >= 30.00)
+            {
+                b="肥満(2度)";
+            }
+            else if(BMI >= 25.00)
+            {
+                b="肥満(1度)";
+            }
+            else if(BMI >= 18.50)
+            {
+                b="普通体重";
+            }
+            else 
+            {
+                b="低体重";
+            }
+
+                    result.Text = "身長" + h + "m、体重" + w + "kgの人のBMIは" + BMI + "です。";
                 }
                 
         }
